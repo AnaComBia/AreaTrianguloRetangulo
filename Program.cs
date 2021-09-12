7f4b408dc2c9986ccb28aba2d89b5f5eaef5993e
AnaComBia <anabeatrizetorres@gmail.com>
2021-09-11 23:49:00 -0300

    finaliza programa Area do triângulo retângulo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AreaTrianguloRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nMe informe a base do triângulo retângulo: ");
+            double Base = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.Write("Me informe a altura do triângulo retângulo: ");
+            double Altura = Convert.ToDouble(Console.ReadLine());
+
+            double Area = Altura * Base / 2;
+
+            Console.WriteLine($"\nA área do triângulo retângulo é: {Area}\n");
+
+        }
+    }
+}
